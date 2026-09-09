cb1d6d16211392009310d04d1f2e3a88704adb21
jacon.xue <jacon.xue@sumscope.com>
2026-09-09 15:46:20 +0800

    feat:支持目录

--- scripts/aa.js
+++
@@ -1,3 +0,0 @@
-(function(){
-    //TODO:params可以获取网页参数66
-})()
--- src/main/resources/META-INF/resources/bb/_next/static/mq66HLmWML1JX7I3Xha1T/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^\\/bb(?:\\/(_next\\/data\\/[^/]{1,}))?\\/bb(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json)?[\\/#\\?]?$",
-    "originalSource": "/bb/:path*"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
